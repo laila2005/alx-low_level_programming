6aa6fed6a6110190e6d7fa91652c20588013ef89
laila <laila.mf2005@gmail.com>
2024-06-23 14:31:23 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #!/bin/bash
 #include <stdio.h>
 
-int main() {
-    // Print the desired message
-    puts("Programming is like building a multilingual puzzle");
+int main(void) {
+ 
+    puts("Programming is like building a multilingual puzzle\n");
 
-    // Return 0 to indicate successful execution
     return 0;
 
 }
