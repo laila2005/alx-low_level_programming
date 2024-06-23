1d8a6b6fe333e720fb1b73d42894b36db892e4d9
laila <laila.mf2005@gmail.com>
2024-06-23 15:36:32 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include <unistd.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, 59); 
-return 1; }
+write(2,message,59); 
+return (1); }
 
