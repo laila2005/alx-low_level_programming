f365f2ae8d9e886c79dddab345035e1364db59a7
laila <laila.mf2005@gmail.com>
2024-06-24 05:10:31 -0700

    task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
 putchar(' ');
 putchar((num2 / 10) + '0');
-putchar((num2 % 10) + '0');            
+putchar((num2 % 10) + '0');
 if (num1 != 98 || num2 != 99)
 {
 putchar(',');
