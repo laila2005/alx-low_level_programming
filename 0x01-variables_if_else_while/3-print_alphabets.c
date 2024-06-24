66f9c36ec1158f9015a756374bb9425b23822110
laila <laila.mf2005@gmail.com>
2024-06-24 03:57:41 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char letterS, letterC;
+
+for (letterS = 'a'; letterS <= 'z'; letterS++)
+{
+putchar(letterS);
+}
+
+for (letterC = 'A'; letterC <= 'Z'; letterC++)
+{
+putchar(letterC);                                                                                      }
+putchar('\n');
+
+return (0); }
