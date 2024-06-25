ee20524c0422aeff72a3eec41212203538dce6fa
laila <laila.mf2005@gmail.com>
2024-06-25 12:19:15 -0700

    task8;

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer, from 00:00 to 23:59.
+ */
 
 void jack_bauer(void)
 {
+int hour, minute;
 
-   
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
+for (hour = 0; hour < 24; hour++)
 {
-jack_bauer();
-return (0); }
+for (minute = 0; minute < 60; minute++)
+{
+printf("%02d:%02d\n", hour, minute);
+}
+}
+}
