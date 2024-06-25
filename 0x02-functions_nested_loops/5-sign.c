99851e595909dc7508379816a8d02a8a8a631b13
laila <laila.mf2005@gmail.com>
2024-06-25 10:11:28 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,12 +18,12 @@ if (n > 0)
 {
 putchar('+');
 return(1); }
-else if (n == 0)
-{
-putchar('0');
-return (0); }
-else
+else if (n < 0)
 {
 putchar('-');
-return(-1); }
+return (-1); }
+else
+{
+putchar('0');
+return(0); }
 return(0); }
