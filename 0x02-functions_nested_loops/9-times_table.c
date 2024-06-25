474e6b2324f982bcc4a30260a33bd5ea5f428682
laila <laila.mf2005@gmail.com>
2024-06-25 13:14:00 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,17 +7,19 @@
 
 void times_table(void)
 {
-int i, j;
+  int i, j;
 
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
 if (j == 0)
-printf("%2d", i * j);
+printf("%d", i * j);
 else
-printf(", %2d", i * j);
+printf(", %d", i * j);
 }
-printf("\n");
+if (i < 9)
+printf(",");
 }
+printf("\n");  
 }
