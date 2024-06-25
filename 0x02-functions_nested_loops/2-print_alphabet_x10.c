7a0f8fef9daea1986e2dab22d5a1f602357b6029
laila <laila.mf2005@gmail.com>
2024-06-25 09:35:57 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - check the code.
@@ -14,13 +14,8 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 26; j++)
 {
-putchar("abcdefghijklmnopqrstuvwxyz"[j]);
+_putchar("abcdefghijklmnopqrstuvwxyz"[j]);
 }
-putchar('\n');
+_putchar('\n');
 }
 }
-
-int main(void)
-{
-print_alphabet_x10();
-return (0); }
