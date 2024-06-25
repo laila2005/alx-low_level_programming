529778476aae3860811f69b1227155d6f3b6bf12
laila <laila.mf2005@gmail.com>
2024-06-25 09:45:28 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - check the code.
@@ -10,21 +10,9 @@ int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
-        return (1); }
+return (1);
+}
 else
 {
-        return (0); }
-}
-
-int main(void)
-{
-int r;
-
-r = _islower('H');
-putchar(r + '0');
-r = _islower('o');
-putchar(r + '0');
-r = _islower(108);
-putchar(r + '0');
-putchar('\n');
 return (0); }
+}
