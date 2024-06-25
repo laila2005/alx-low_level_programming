74bb687bf10a1ab97d3862087e981b6306602c34
laila <laila.mf2005@gmail.com>
2024-06-25 04:08:47 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return(1);
+}
+else 
+{
+return(0);
+}
+}
+
+int main(void)
+{
+    int r;
+
+r = _isalpha('H');
+putchar(r + '0');
+r = _isalpha('o');
+putchar(r + '0');
+r = _isalpha(108);
+putchar(r + '0');
+r = _isalpha(';');
+putchar(r + '0');
+putchar('\n');
+return (0); }
