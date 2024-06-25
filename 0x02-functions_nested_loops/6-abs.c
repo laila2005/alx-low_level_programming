d182153020d3bc0413f4aa15d91912730631e84c
laila <laila.mf2005@gmail.com>
2024-06-25 04:33:28 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int _abs(int n)
+{
+    if (n < 0)
+    {
+        return (-n);
+    }
+    else
+    {
+        return (n);
+    }
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _abs(-1);
+    printf("%d\n", r);
+    r = _abs(0);
+    printf("%d\n", r);
+    r = _abs(1);
+    printf("%d\n", r);
+    r = _abs(-98);
+    printf("%d\n", r);
+    
+    return (0);
+}
