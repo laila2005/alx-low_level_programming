d6656f30824d2c64cff31484588f70c91168914b
laila <laila.mf2005@gmail.com>
2024-06-28 14:56:28 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints numbers from 1 to 100, replacing multiples
+ * of 3 with "Fizz", multiples of 5 with "Buzz", and multiples
+ * of both 3 and 5 with "FizzBuzz".
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	  int i;
