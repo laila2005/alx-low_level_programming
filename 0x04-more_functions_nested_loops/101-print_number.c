e329a8a2e5d4f12b0e62d9b6b98348984152850c
laila <laila.mf2005@gmail.com>
2024-06-28 17:16:09 -0700

    task12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,22 +2,30 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer using only _putchar.
- * @n: The integer to be printed.
- *
- * Description: This function prints an integer character by character
- * using recursion. It handles negative numbers by printing a '-' sign
- * and then converting the number to positive. The function recursively
- * divides the number by 10 to print each digit from the most significant
- * to the least significant, and uses _putchar to print each character.
- */
+* print_number - Prints an integer using only _putchar.
+* @n: The integer to be printed.
+*
+* Description: This function prints an integer character by character
+* using recursion. It handles negative numbers by printing a '-' sign
+* and then converting the number to positive. The function recursively
+* divides the number by 10 to print each digit from the most significant
+* to the least significant, and uses _putchar to print each character.
+*/
 
 void print_number(int n)
 {
 if (n < 0)
 {
 _putchar('-');
+if (n == -2147483648)
+{
+_putchar('2');
+n = 147483648;
+}
+else
+{
 n = -n;
+}
 }
 if (n / 10)
 {
