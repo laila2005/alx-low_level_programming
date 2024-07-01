854f730d53eb5f5d24feb55792ada70c0a5bee93
laila <laila.mf2005@gmail.com>
2024-07-01 15:10:32 -0700

    task10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,23 +3,24 @@
 #include <stdio.h>
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: Pointer to the string to be converted.
- *
- * Return: The converted integer.
- */
+* _atoi - Converts a string to an integer.
+* @s: Pointer to the string to be converted.
+*
+* Return: The converted integer.
+*/
 
 int _atoi(char *s)
 {
 int sign = 1;
 int result = 0;
 int i = 0;
+int started = 0;
 
 while (s[i] == ' ')
 {
 i++;
 }
-while (s[i] == '-' || s[i] == '+' || s[i] == ' ')
+while (s[i] == '-' || s[i] == '+')
 {
 if (s[i] == '-')
 {
@@ -27,19 +28,23 @@ sign = -sign;
 }
 i++;
 }
-while (s[i] >= '0' && s[i] <= '9')
+while (s[i] != '\0')
 {
-if (result > INT_MAX / 10 || (result == INT_MAX / 10
-&& (s[i] - '0') > INT_MAX % 10))
+if (s[i] >= '0' && s[i] <= '9')
+{
+started = 1;
+if (result > INT_MAX / 10 || (result == INT_MAX / 10 &&
+(s[i] - '0') > INT_MAX % 10))
 {
 return ((sign == 1) ? INT_MAX : INT_MIN);
 }
 result = result * 10 + (s[i] - '0');
-i++;
 }
-if (s[i] != '\0')
+else if (started)
 {
-return (result *sign);
+break;
+}
+i++;
 }
 return (result *sign);
 }
