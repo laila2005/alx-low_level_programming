3b80af050d4ac9270bceaf5a31e253d4c745742b
laila <laila.mf2005@gmail.com>
2024-07-01 11:54:36 -0700

    task10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,31 +11,35 @@
 
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int i = 0;
+int sign = 1;
+int result = 0;
+int i = 0;
 
-    while (s[i] == ' ')
-    {
-        i++;
-    }
-    while (s[i] == '-' || s[i] == '+')
-    {
-        if (s[i] == '-')
-        {
-            sign = -sign;
-        }
-        i++;
-    }
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        if (result > INT_MAX / 10 || (result == INT_MAX / 10
-            && (s[i] - '0') > INT_MAX % 10))
-        {
-            return ((sign == 1) ? INT_MAX : INT_MIN);
-        }
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
-    return (result * sign);
+while (s[i] == ' ')
+{
+i++;
+}
+while (s[i] == '-' || s[i] == '+' || s[i] == ' ')
+{
+if (s[i] == '-')
+{
+sign = -sign;
+}
+i++;
+}
+while (s[i] >= '0' && s[i] <= '9')
+{
+if (result > INT_MAX / 10 || (result == INT_MAX / 10
+&& (s[i] - '0') > INT_MAX % 10))
+{
+return ((sign == 1) ? INT_MAX : INT_MIN);
+}
+result = result * 10 + (s[i] - '0');
+i++;
+}
+if (s[i] != '\0')
+{
+return (result *sign);
+}
+return (result *sign);
 }
