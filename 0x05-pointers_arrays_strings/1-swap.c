739d96ecdb76f58e226c902af99a52ca97e9a5c5
laila <laila.mf2005@gmail.com>
2024-07-01 08:30:55 -0700

    task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,11 +5,12 @@
  * @a: Pointer to the first integer.
  * @b: Pointer to the second integer.
  */
+
 void swap_int(int *a, int *b)
 {
-    int temp;
+int temp;
 
-    temp = *a;
-    *a = *b;
-    *b = temp;
+temp = *a;
+*a = *b;
+*b = temp;
 }
