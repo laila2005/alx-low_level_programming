d86bee15b54a7a4176777ac212004c71f371ff0f
laila <laila.mf2005@gmail.com>
2024-07-04 06:13:57 -0700

    task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+char *dest_end = dest;
+while (*dest_end != '\0')
+{
+dest_end++;
+}
+while (*src != '\0')
+{
+*dest_end = *src;
+src++;
+}
+*dest_end = '\0';
+return (dest);
+}
