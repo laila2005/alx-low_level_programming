3da4516900abeccd66965cd53a720f20b3841b9a
laila <laila.mf2005@gmail.com>
2024-07-05 09:38:01 -0700

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* _strncat - Concatenates at most n bytes from src string to dest string
+* @dest: The destination string to which the src string will be appended
+* @src: The source string to be appended to the dest string
+* @n: The maximum number of bytes to be appended from src
+*
+* Description: This function appends at most n bytes from the src string to the
+* dest string, overwriting the terminating null byte (\0) at the end of dest,
+* and then adds a terminating null byte. If src contains
+* n or more bytes, it does
+* not need to be null-terminated.The function returns a pointer to
+* string dest. Ensure that dest has enough space to hold the result of the
+* concatenation to avoid buffer overflow.
+*
+* Return: A pointer to the resulting string dest.
+*/
+
+char *_strncat(char *dest, const char *src, int n)
+{
+char *dest_end = dest;
+while (*dest_end != '\0')
+{
+dest_end++;
+}
+while (n > 0 && *src != '\0')
+{
+*dest_end = *src;
+dest_end++;
+src++;
+n--;
+}
+*dest_end = '\0';
+return (dest); }
