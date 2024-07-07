70722a36e56db84ae3933b4e7a1f2295a5a8cc4d
laila <laila.mf2005@gmail.com>
2024-07-07 15:32:13 -0700

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* _memcpy - Copies at most n bytes from src string to dest string
+* @dest: The destination string to which the src string will be copied
+* @src: The source string to be copied to the dest string
+* @n: The maximum number of bytes to be copied from src
+*
+* Description: This function copies at most n bytes from the src string to the
+* dest string. If src contains less than n bytes, the rest of dest will be
+* padded with null bytes. If src contains n or more bytes, dest will not be
+* null-terminated.
+*
+* Return: A pointer to the resulting string dest.
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
+}
