63e2b2c7cdb8ef49f1522323cdcc2c99c570d99e
laila <laila.mf2005@gmail.com>
2024-07-10 14:49:21 -0700

    task101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,46 +1,59 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* wildcmp - compares two strings considering special character *
-* @s1: The first string to compare
-* @s2: The second string to compare, can contain *
-*
-* Description: This function compares two strings,where s2 can contain
-* the special character * that can replace any string
-* (including an empty string).
-* It uses recursion to handle the comparison.
-*
-* Return: 1 if the strings can be considered identical, otherwise 0
-*/
+ * _wildcmp - recursively compares two strings considering wildcard *
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string, may contain '*'
+ *
+ * Description:
+ * This function recursively compares two strings, where s2 can contain
+ * the special character '*' that can match with any sequence of characters
+ * (including an empty sequence). It uses recursion to handle the comparison
+ * without loops.
+ *
+ * Return:
+ * 1 if the strings can be considered identical, otherwise 0
+ */
 
-int wildcmp(char *s1, char *s2)
+int _wildcmp(char *s1, char *s2)
 {
 if (*s1 == '\0' && *s2 == '\0')
 {
 return (1);
 }
-if (*s2 == '*')
-{
-while (*s2 == '*')
-{
-s2++;
-}
-if (*s2 == '\0')
+if (*s2 == '*') {
+if (*(s2 + 1) == '\0')
 {
 return (1);
 }
-while (*s1 != '\0')
+if (*s1 == '\0')
 {
-if (wildcmp(s1, s2))
-{
-return (1);
+return (0);
 }
-s1++;
-}
+return (_wildcmp(s1 + 1, s2) || _wildcmp(s1, s2 + 1));
 }
 if (*s1 == *s2)
 {
-return (wildcmp(s1 + 1, s2 + 1));
+return (_wildcmp(s1 + 1, s2 + 1));
 }
 return (0);
+}
+
+/**
+ * wildcmp - compares two strings considering wildcard *
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string, may contain '*'
+ *
+ * Description:
+ * This function compares two strings, where s2 can contain
+ * the special character '*' that can match with any sequence of characters
+ * (including an empty sequence). It serves as a wrapper for _wildcmp.
+ *
+ * Return:
+ * 1 if the strings can be considered identical, otherwise 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+return (_wildcmp(s1, s2));
 }
