562b180ce22520d92d9f5a37ae8866961dc39034
laila <laila.mf2005@gmail.com>
2024-07-22 15:08:31 -0700

    task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,64 +1,18 @@
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
 void print_dog(struct dog *d)
 {
-    char *nil_str = "(nil)";
-    int int_age, digit, decimal_age;
-
-    if (d == NULL)
-    {
-     
-        return;
-    }
-
-        if (d->name != NULL)
-    {
-        char *name = d->name;
-        while (*name != '\0')
-	{
-            _putchar(*name);
-            name++;
-        }
-    }
-    else
-    {
-        while (*nil_str != '\0')
-	{
-            _putchar(*nil_str);
-            nil_str++;
-        }
-    }
-    _putchar('\n');
-
-    int_age = (int)d->age;
-    int_age = (int_age >= 0) ? int_age : -int_age;
-    while (int_age > 0) {
-        digit = int_age % 10;
-        _putchar('0' + digit);
-        int_age /= 10;
-    }
-    _putchar('.');
-    decimal_age = (int)((d->age - (float)int_age) * 10);
-    _putchar('0' + decimal_age);
-    _putchar('\n');
-
-    
-    if (d->owner != NULL)
-    {
-        char *owner = d->owner;
-        while (*owner != '\0')
-	{
-            _putchar(*owner);
-            owner++;
-        }
-    }
-    else
-    {
-        while (*nil_str != '\0')
-	{
-            _putchar(*nil_str);
-            nil_str++;
-        }
-    }
-    _putchar('\n');
-}
+if (d == NULL)
+return;
+if (d->name == NULL)
+d->name = "(nil)";
+if (d->owner == NULL)
+d->owner = "(nil)";
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name,
+d->age, d->owner); }
