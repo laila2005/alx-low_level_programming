e645227ce150c20da27db712e16d1f9117d6a1a6
laila <laila.mf2005@gmail.com>
2024-08-06 11:15:45 -0700

    task6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,22 +3,25 @@
 #include "lists.h"
 
 /**
-* pop_listint - Deletes the head node of a listint_t linked list.
+* pop_listint - Deletes the head node of a listint_t linked list and returns its data.
 * @head: A pointer to a pointer to the head of the listint_t list.
 *
-* Return: The data of the deleted head node (n), or 0 if the list is empty.
+* Return: The data of the deleted head node, or 0 if the list is empty.
 *
-* Description: This function removes the head node of a linked list, frees its
-* memory, and returns its data. If the list is empty, it returns 0.
+* Description: This function removes the head node from the linked list, updates
+* the head pointer, and returns the data stored in the removed node. If the list is
+* empty, it returns 0.
 */
 int pop_listint(listint_t **head)
 {
+listint_t *temp;
+int data;
+
 if (head == NULL || *head == NULL)
 return (0);
 
-listint_t *temp = *head;
-int data = temp->n;
-
+temp = *head;
+data = temp->n;
 *head = (*head)->next;
 free(temp);
 
