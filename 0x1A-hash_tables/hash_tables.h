2dc0dc9e4986372e3e1d2c831c29065b946e8422
laila <laila.mf2005@gmail.com>
2024-10-12 17:48:25 -0700

    task1

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -34,4 +34,5 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);  /* Declare hash_djb2 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 hash_node_t *hash_node_create(const char *key, const char *value);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 #endif
